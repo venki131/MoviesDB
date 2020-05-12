b55fa6d8bd8048a638e8cf33dfbce292360e44ca
Venkatesh Kashyap <venkatesh.kashyap13@gmail.com>
2020-05-12 20:32:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/moviesdb/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.moviesdb
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initSearch()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val subMenu = menu?.getItem(0)?.subMenu
+        if (subMenu?.equals("Movies")!!) {
+            subMenu.item.isChecked = true
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun initSearch() {
+        val manager: SearchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem: SearchView = toolbar.menu.findItem(R.id.search).actionView as SearchView
+        toolbar.menu.findItem(R.id.filter).subMenu.item.isChecked = true
+        searchItem.setSearchableInfo(manager.getSearchableInfo(componentName))
+        searchItem.queryHint = "Search"
+        searchItem.isIconified = false
+        searchItem.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchItem.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                if (query?.length!! >= 3) {
+                    println(query)
+                }
+                return true
+            }
+        })
+    }
+}
